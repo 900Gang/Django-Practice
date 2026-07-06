3cf52cdfd1ec368144dd10cb2ac5666f78529e4e
900Gang <anandanand6776@gmail.com>
2026-07-06 22:17:43 +0530

    Addded

--- second_project/second_app/admin.py
+++ second_project/second_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from second_app.models import Topic,webpage,AccessRecord
 
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(webpage)
+admin.site.register(AccessRecord)
